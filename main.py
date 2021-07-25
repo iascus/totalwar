5012ce1d69c3ad728f03d324419941a421390dd5
iascus <iascus.publicus@gmail.com>
2021-07-25 21:44:51 +0100

    MK II

--- main.py
+++ main.py
@@ -14,16 +14,56 @@ def read_tsv(filename):
     )
 
 
+def write_tsv(df, filename, header):
+    for col in df.columns[df.dtypes == 'bool']:
+        df.loc[:, col] = df.loc[:, col].astype(str).str.lower()
+
+    with open(os.path.join(tsv_path, filename), 'wb') as buf:
+        buf.write(f'{header}\n'.encode('utf8'))
+        df.to_csv(
+            buf,
+            sep='\t',
+            index=False,
+            line_terminator='\n',
+        )
+
+
 def main():
     pd.set_option(
         'display.width', None,
-        'display.max_rows', 200,
+        'display.max_rows', 500,
         'display.max_columns', 200,
     )
-    land_units = read_tsv('land_units_tables.tsv')
-    main_units = read_tsv('main_units_tables.tsv')
+    land_units = read_tsv('land_units_tables_old.tsv')
+    main_units = read_tsv('main_units_tables_old.tsv')
     print(land_units)
     print(main_units)
+
+    both = pd.merge(main_units, land_units, left_on='land_unit', right_on='key')
+    single_entity = both[
+        (
+                both.use_hitpoints_in_campaign
+                |
+                ( both.num_engines == 1 )
+                |
+                both.num_men.isin([1, 5])
+        )
+        &
+        (
+            both.land_unit != 'teb_galloper_horse'
+        )
+    ]
+    engines = both[~both.unit.isin(single_entity.unit) & (both.num_engines > 0)]
+    non_engines = both[~both.unit.isin(single_entity.unit) & (both.num_engines == 0)]
+
+    land_units.loc[land_units.key.isin(single_entity.land_unit), ['bonus_hit_points']] *= 2
+    land_units.loc[land_units.key.isin(engines.land_unit), ['num_engines']] *= 2
+    land_units.loc[land_units.key.isin(non_engines.land_unit), ['num_mounts']] *= 2
+    main_units.loc[main_units.unit.isin(engines.unit), ['num_men']] *= 2
+    main_units.loc[main_units.unit.isin(non_engines.unit), ['num_men']] *= 2
+
+    write_tsv(land_units, 'land_units_tables.tsv', 'land_units_tables	44')
+    write_tsv(main_units, 'main_units_tables.tsv', 'main_units_tables	36')
 
 
 if __name__ == '__main__':
